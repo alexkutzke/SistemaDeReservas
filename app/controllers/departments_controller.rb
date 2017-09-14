110c03d04f3b6a2e2390427931551e24b8e9d1b7
Lais Frigério <laaisfrigerio@gmail.com>
2017-09-14 16:36:41 -0300

    Issue #4 / Sprint 7 - Add departments API routes to json and html format

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,6 +1,8 @@
 class DepartmentsController < ApplicationController
-    before_action :set_department, only: [:update, :destroy, :edit]
+    before_action :set_department, only: [:show, :update, :destroy, :edit]
 
+    # GET /departments
+    # GET /departments.json
     def index
         @departments = Department.paginate(:page => params[:page], per_page:5)
         @number = Department.number_of_records
@@ -11,33 +13,60 @@ class DepartmentsController < ApplicationController
         end
     end
 
+    # GET /departments/new
     def new
         @department = Department.new
     end
 
+    # GET /departments/1.json
+    def show
+        respond_to do |format|
+            format.json { render json: @department }
+        end
+    end
+
+    # GET /departments/edit/1
+    # GET /departments/edit/1.json
     def edit
     end
 
+    # POST /departments
+    # POST /departments.json
     def create
         @department = Department.new(department_params)
-        if @department.save
-            redirect_to departments_path
-        else
-            render 'new'
+        respond_to do |format|
+            if @department.save
+                format.html { render :index }
+                format.json { render json:  @department, status: :created}
+            else
+                format.html { render :new }
+                format.json { render json: @department.errors, status: :unprocessable_entity }
+            end
         end
     end
 
+    # PATCH/PUT /departments/1
+    # PATCH/PUT /departments/1.json
     def update
-        if @department.update(department_params)
-            redirect_to departments_pat
-        else
-            render 'edit'
+        respond_to do |format|
+            if @department.update(department_params)
+                format.html { render :index}
+                format.json { render json: @department, status: :ok }
+            else
+                format.html { render :edit }
+                format.json { render json:  @department.errors, status: :unprocessable_entity }
+            end
         end
     end
 
+    # DELETE /departments/1
+    # DELETE /departments/1.json
     def destroy
         @department.destroy
-        redirect_to departments_path
+        respond_to do |format|
+            format.html { redirect_to departments_url, notice: 'Task was successfully destroyed.' }
+            format.json { head :no_content }
+        end
     end
 
     private
