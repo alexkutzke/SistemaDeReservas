3090b397a39bfddd7c548c2b13a1179e0d98f848
Lais Frigerio <laaisfrigerio@gmail.com>
2017-09-19 12:03:23 -0300

    Issue #9 / Sprint 7 - Add manter setores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919141026) do
+ActiveRecord::Schema.define(version: 20170919144451) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -71,6 +71,12 @@ ActiveRecord::Schema.define(version: 20170919141026) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.index ["category_id"], name: "index_rooms_on_category_id"
+  end
+
+  create_table "sectors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "student_classes", force: :cascade do |t|
