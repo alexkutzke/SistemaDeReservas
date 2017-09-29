7b1812767695c6c8adc296d3ad96f13c795de1cb
Lais Frigério <laaisfrigerio@gmail.com>
2017-09-29 18:50:57 -0300

    Issue #10 - Add test from discipline model
    Signed-off-by: Lais Frigério <laaisfrigerio@gmail.com>

--- test/models/discipline_test.rb
+++ test/models/discipline_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class DisciplineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # save discipline
+  test "save discipline" do
+      discipline = Discipline.new
+      discipline.name = "Introdução a banco de dados"
+      discipline.discipline_code = "TI123"
+      discipline.department_id = 1
+      assert discipline.save
+  end
+
+  # Should not save (empty fields)
+  test "shoud not save discipline without name" do
+      discipline = Discipline.new
+      discipline.discipline_code = "TI123"
+      discipline.department_id = 1
+      assert_not discipline.save
+  end
+
+  test "shoud not save discipline without discipline code" do
+      discipline = Discipline.new
+      discipline.name = "Introdução a banco de dados"
+      discipline.department_id = 1
+      assert_not discipline.save
+  end 
+
+  test "shoud not save discipline without department id" do
+      discipline = Discipline.new
+      discipline.name = "Introdução a banco de dados"
+      discipline.discipline_code = "TI123"
+      assert_not discipline.save
+  end 
+
+  # Should not save (minimum length required) 
+  test "should not save discipline  = name less than 3 characters" do
+      discipline = Discipline.new
+      discipline.name = "In"
+      discipline.discipline_code = "TI123"
+      discipline.department_id = 1
+      assert_not discipline.save
+  end
+
+  test "should not save discipline  = discipline code less than 3 characters" do
+      discipline = Discipline.new
+      discipline.name = "Introdução a banco de dados"
+      discipline.discipline_code = "23"
+      discipline.department_id = 1
+      assert_not discipline.save
+  end
+
+  test "should not save discipline  = department id less than 1 characters" do
+      discipline = Discipline.new
+      discipline.discipline_code = "TI123"
+      discipline.department_id = 
+      assert_not discipline.save
+  end
 end
