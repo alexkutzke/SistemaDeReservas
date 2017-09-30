b7e5d240cb64cdf12da798046923e77affd5053f
Lais Frigério <laaisfrigerio@gmail.com>
2017-09-30 11:15:43 -0300

    Issue #10 - Add test from classroom model
    Signed-off-by: Lais Frigério <laaisfrigerio@gmail.com>

--- test/models/classroom_test.rb
+++ test/models/classroom_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class ClassroomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # save classroom
+  test "save classroom" do
+      classroom = Classroom.new
+      classroom.room = "A10"
+      classroom.building = "Bloco A"
+      classroom.capacity = 30
+      classroom.category_id = 1
+      assert classroom.save
+  end
+
+  # should not save classroom (empty fields)
+  # minimum length required = 1
+  test "should not save classroom without a room" do 
+    classroom = Classroom.new
+    classroom.building = "Bloco A"
+    classroom.capacity = 30
+    classroom.category_id = 1
+    assert_not classroom.save
+  end
+
+  test "should not save classroom without a building" do 
+    classroom = Classroom.new
+    classroom.room = "A01"
+    classroom.capacity = 30
+    classroom.category_id = 1
+    assert_not classroom.save
+  end
+
+  test "should not save classroom without a capacity" do 
+    classroom = Classroom.new
+    classroom.room = "A01"
+    classroom.building = "Bloco A"
+    classroom.category_id = 1
+    assert_not classroom.save
+  end
+
+  test "should not save classroom without a category id" do 
+    classroom = Classroom.new
+    classroom.room = "A01"
+    classroom.building = "Bloco A"
+    classroom.capacity = 30
+    assert_not classroom.save
+  end
 end
