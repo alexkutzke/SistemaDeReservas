226465abd852c2f62bd29a7b0483b52e5bc2f5c8
Lais Frigério <lais@polvo.com.br>
2017-11-20 07:55:11 -0200

    Add data inicio e data fim obrigatorios
    Signed-off-by: Lais Frigério <lais@polvo.com.br>

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,5 +1,7 @@
 class Period < ApplicationRecord
   has_many :klasses
   validates :name, presence: true, length: { minimum:3 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   # validate dates (start and end)
 end
